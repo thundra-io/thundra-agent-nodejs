c7dff97c6f59bc0819a9c002494241918bd42d35
İbrahim Gürses <ibrahim.gurses@opsgenie.com>
2018-11-14 16:32:37 +0300

    Update THUNDRA_APPLICATION_ID

--- src/Constants.ts
+++ src/Constants.ts
@@ -40,7 +40,7 @@ export const envVariableKeys = {
     THUNDRA_AGENT_METRIC_COUNT_AWARE_SAMPLER_COUNT_FREQ: 'thundra_agent_lambda_metric_sample_sampler_countAware_countFreq',
 
     THUNDRA_APPLICATION_TAG_PROP_NAME_PREFIX: 'thundra_agent_lambda_application_tag_',
-    THUNDRA_APPLICATION_ID: 'thundra_agent_application_id',
+    THUNDRA_APPLICATION_ID: 'thundra_agent_lambda_application_id',
     THUNDRA_APPLICATION_NAME: 'thundra_agent_lambda_application_name',
 
 };
