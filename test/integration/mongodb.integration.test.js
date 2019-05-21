504c7e1267e81b798a575b76dc90d6e94e299bec
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-05-21 17:39:31 +0300

    Add update test

--- test/integration/mongodb.integration.test.js
+++ test/integration/mongodb.integration.test.js
@@ -16,7 +16,7 @@ describe('MongoDB Integration', () => {
             colors: ["gray", "black", "white"],
         };
 
-        return MongoDB.insert(doc, sdk).then((data) => {
+        return MongoDB.insert(doc, sdk).then(() => {
             const span = tracer.getRecorder().spanList[0];
             
             expect(span.className).toBe('MONGODB');
@@ -39,5 +39,39 @@ describe('MongoDB Integration', () => {
             let command = JSON.parse(span.tags['mongodb.command'])
             expect(command.documents[0].name).toBe('foo');
         });
-    })
+    });
+    
+    test('should instrument MongoDB update calls', () => {
+        const integration = new MongoDBIntegration({});
+        const sdk = require('mongodb');
+
+        const tracer = new ThundraTracer();
+        const doc = {
+            name: 'foo',
+            colors: ['gray', 'black', 'white'],
+        };
+
+        return MongoDB.update(doc, {name: 'foo'}, sdk).then(() => {
+            const span = tracer.getRecorder().spanList[1];
+            expect(span.className).toBe('MONGODB');
+            expect(span.domainName).toBe('DB');
+            expect(span.operationName).toBe('UPDATE');
+            expect(span.tags['operation.type']).toBe('WRITE');
+            expect(span.tags['db.host']).toBe('localhost');
+            expect(span.tags['db.port']).toBe('27017');
+            expect(span.tags['db.instance']).toBe('testDB');
+            expect(span.tags['db.type']).toBe('mongodb');
+            
+            expect(span.tags['mongodb.command.name']).toBe('UPDATE');
+            expect(span.tags['mongodb.collection.name']).toBe('testCollection');
+
+            expect(span.tags['topology.vertex']).toEqual(true);
+            expect(span.tags['trigger.domainName']).toEqual('API');
+            expect(span.tags['trigger.className']).toEqual('AWS-Lambda');
+            expect(span.tags['trigger.operationNames']).toEqual(['functionName']);
+
+            let command = JSON.parse(span.tags['mongodb.command']);
+            expect(command.updates[0].q.name).toBe('foo');
+        });
+    });
 })
--- test/integration/utils/mongodb.integration.utils.js
+++ test/integration/utils/mongodb.integration.utils.js
@@ -7,11 +7,35 @@ module.exports.insert = (doc, mongodb) => {
         MongoClient.connect(mongoURL, {useNewUrlParser: true }, (err, client) => {
             const db = client.db(dbName);
             const collection = db.collection(collectionName);
-            collection.insertOne(doc).then((res) => {
-                return resolve(data);
-            }).catch((err) => {
-                return resolve(err);
-            });
+            collection.insertOne(doc)
+            .then((res) => client.close())
+            .then(() => resolve())
+            .catch((err) => {
+                client.close()
+                .then(resolve);
+            })
+        });
+    });
+}
+
+module.exports.update = (doc, updateKey, mongodb) => {
+    return new Promise((resolve) => {
+        const MongoClient = mongodb.MongoClient;
+        const mongoURL = 'mongodb://localhost:27017';
+        const dbName = 'testDB';
+        const collectionName = 'testCollection'
+        MongoClient.connect(mongoURL, {useNewUrlParser: true }, (err, client) => {
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+            collection.insertOne(doc)
+            .then((res) => collection.updateOne(updateKey, {
+                '$set': { 'newField': 'newValue '},
+            })).then((res) => client.close())
+            .then(() => resolve())
+            .catch((err) => {
+                client.close()
+                .then(resolve);
+            })
         });
     });
 }
