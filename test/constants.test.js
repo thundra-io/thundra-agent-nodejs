31123e5135db6b06f04416622890a76782dc23d2
Serkan ÖZAL <serkan@thundra.io>
2019-08-02 13:37:26 +0300

    Decided collector API endpoint based on current region adaptively (#93)
    * Decided collector API endpoint based on current region adaptively
    
    * Fixed lint issue

--- test/constants.test.js
+++ test/constants.test.js
@@ -1,4 +1,4 @@
-import {HOOKS, URL, PROC_STAT_PATH, PROC_IO_PATH, DATA_MODEL_VERSION, getTimeoutMargin} from '../dist/Constants';
+import {HOOKS, URL, PROC_STAT_PATH, PROC_IO_PATH, DATA_MODEL_VERSION, getTimeoutMargin, getAPIEndpoint} from '../dist/Constants';
 
 test('DATA_MODEL_VERSION did not change', () => {
     expect(DATA_MODEL_VERSION).toEqual('2.0');
@@ -48,5 +48,30 @@ test('Timeout margin should be decided based on region', () => {
     expect(getTimeoutMargin()).toEqual(1000); // Invalid
 });
 
+test('API endpoint should be decided based on region', () => {
+    expect(getAPIEndpoint('us-west-2')).toEqual('api.thundra.io');
+    expect(getAPIEndpoint('us-west-1')).toEqual('api.thundra.io');
 
+    expect(getAPIEndpoint('us-east-2')).toEqual('api-us-east-1.thundra.io');
+    expect(getAPIEndpoint('us-east-1')).toEqual('api-us-east-1.thundra.io');
 
+    expect(getAPIEndpoint('eu-central-1')).toEqual('api-eu-west-2.thundra.io');
+    expect(getAPIEndpoint('eu-west-1')).toEqual('api-eu-west-2.thundra.io');
+    expect(getAPIEndpoint('eu-west-2')).toEqual('api-eu-west-2.thundra.io');
+    expect(getAPIEndpoint('eu-west-3')).toEqual('api-eu-west-2.thundra.io');
+    expect(getAPIEndpoint('eu-north-1')).toEqual('api-eu-west-2.thundra.io');
+
+    expect(getAPIEndpoint('ca-central-1')).toEqual('api-us-east-1.thundra.io');
+
+    expect(getAPIEndpoint('sa-east-1')).toEqual('api-us-east-1.thundra.io');
+
+    expect(getAPIEndpoint('ap-south-1')).toEqual('api-ap-northeast-1.thundra.io');
+    expect(getAPIEndpoint('ap-northeast-2')).toEqual('api-ap-northeast-1.thundra.io');
+    expect(getAPIEndpoint('ap-southeast-1')).toEqual('api-ap-northeast-1.thundra.io');
+    expect(getAPIEndpoint('ap-southeast-2')).toEqual('api-ap-northeast-1.thundra.io');
+    expect(getAPIEndpoint('ap-northeast-1')).toEqual('api-ap-northeast-1.thundra.io');
+
+    expect(getAPIEndpoint('tr-east-1')).toEqual('api.thundra.io'); // Unknown
+
+    expect(getAPIEndpoint()).toEqual('api.thundra.io'); // Invalid
+});
