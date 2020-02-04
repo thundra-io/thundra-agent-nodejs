cc1bc8a25f85d1aef1e508d21e494c42a040dfb1
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-02-04 22:41:20 +0300

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -4,12 +4,12 @@ import { loadHandler } from './runtime/RuntimeSupport';
 
 const HANDLER_ENV_VAR = 'thundra_agent_lambda_handler';
 
+const thundra = require('@thundra/core')();
 const userHandler = loadHandler(
   process.env.LAMBDA_TASK_ROOT,
   process.env[HANDLER_ENV_VAR],
 );
 
-const thundra = require('@thundra/core')();
 const wrappedUserFunc = thundra(userHandler);
 
 exports.wrapper = (event: any, context: any, callback: any) => {
