996a055293f4f6c50d61d0fba9590695e2f64389
Fatih Aydilek <fatihaydilek@gmail.com>
2020-02-28 14:24:57 +0300

    add normalizeFunctionName (#140)
    * add normalizeFunctionName
    
    * add request as a qualifier source

--- src/plugins/utils/Utils.ts
+++ src/plugins/utils/Utils.ts
@@ -477,6 +477,24 @@ class Utils {
             segmentID,
         };
     }
+
+    static normalizeFunctionName(fullName: string) {
+        const parts = fullName.split(':');
+
+        if (parts.length === 0 || parts.length === 1) { // funcName
+            return {name: fullName};
+        } else if (parts.length === 2) { // funcName:qualifier
+            return {name: parts[0], qualifier: parts[1]};
+        } else if (parts.length === 3) { // accountId:function:funcName
+            return {name: parts[2]};
+        } else if (parts.length === 4) { // accountId:function:funcName:qualifier
+            return {name: parts[2], qualifier: parts[3]};
+        } else if (parts.length === 7) { // arn:aws:lambda:region:accountId:function:funcName
+            return {name: parts[6]};
+        } else if (parts.length === 8) { // arn:aws:lambda:region:accountId:function:funcName:qualifier
+            return {name: parts[6], qualifier: parts[7]};
+        }
+    }
 }
 
 export default Utils;
