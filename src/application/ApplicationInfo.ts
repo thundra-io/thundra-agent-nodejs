b145bf01c7c9b633ce3b08ea88f6d9c851e26b07
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-06-12 16:14:03 +0300

    Clean up (#172)

--- src/application/ApplicationInfo.ts
+++ src/application/ApplicationInfo.ts
@@ -1,7 +1,3 @@
-import ConfigProvider from '../config/ConfigProvider';
-import ConfigNames from '../config/ConfigNames';
-import ThundraLogger from '../ThundraLogger';
-
 export interface ApplicationInfo {
     applicationId: string;
     applicationInstanceId: string;
