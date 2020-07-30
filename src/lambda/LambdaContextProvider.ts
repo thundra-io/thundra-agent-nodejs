375e84f24fa6a5565a4af5ccf202baedaff51226
Serkan ÖZAL <serkan@thundra.io>
2020-07-31 01:25:23 +0300

    Added comment to `LambdaContextProvider.ts`

--- src/lambda/LambdaContextProvider.ts
+++ src/lambda/LambdaContextProvider.ts
@@ -5,12 +5,20 @@ export class LambdaContextProvider {
 
     static context: any;
 
-    static setContext(context: any) {
-        LambdaContextProvider.context = context;
-    }
-
+    /**
+     * Gets the AWS Lambda invocation context
+     * @return the AWS Lambda invocation context
+     */
     static getContext() {
         return LambdaContextProvider.context;
+    }
+
+    /**
+     * Sets the AWS Lambda invocation context
+     * @param context the AWS Lambda invocation context to be set
+     */
+    static setContext(context: any) {
+        LambdaContextProvider.context = context;
     }
 
 }
