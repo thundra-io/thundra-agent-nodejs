c77a96f2719e989dcc17cc5b90bba8bbd0b5e61d
Serkan ÖZAL <serkan@thundra.io>
2020-07-30 22:20:38 +0300

    Added comment to `custom.d.ts`

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,7 +1,13 @@
+/**
+ * Defines custom types
+ */
+
 declare namespace NodeJS {
+
     export interface Global {
         __thundraTraceEntry__ : any
         __thundraTraceLine__ : any
         __thundraTraceExit__ : any
     }
+
 }
