2a166a21c2d45a668577e499a45474a923314501
Serkan ÖZAL <serkan@thundra.io>
2020-07-30 21:54:56 +0300

    Added comment to `ExecutionContextManager.ts`

--- src/context/ExecutionContextManager.ts
+++ src/context/ExecutionContextManager.ts
@@ -3,12 +3,41 @@ import * as globalContextProvider from './globalContextProvider';
 
 let contextProvider = globalContextProvider;
 
+/**
+ * Manages {@link ExecutionContext} and
+ * contract point to the outside for {@link ExecutionContext} related operation.
+ */
 export default class ExecutionContextManager {
 
+    /**
+     * Initializes {@link ExecutionContextManager}
+     */
+    static init() {
+        contextProvider.init();
+    }
+
+    /**
+     * Sets the {@link ExecutionContext} provider
+     * @param provided the {@link ExecutionContext} provider to be set
+     */
+    static setProvider(provider: any) {
+        contextProvider = provider;
+    }
+
+    /**
+     * Runs given {@param fn} in the created {@link ExecutionContext}
+     * by given {@param createExecContext}
+     * @param {Function} createExecContext creates {@link ExecutionContext}
+     * @param {Function} fn runs in the created {@link ExecutionContext}
+     */
     static runWithContext(createExecContext: Function, fn: Function) {
         return contextProvider.runWithContext(createExecContext, fn);
     }
 
+    /**
+     * Gets the {@link ExecutionContext}
+     * @return {ExecutionContext} the {@link ExecutionContext}
+     */
     static get(): ExecutionContext {
         const execContext: ExecutionContext = contextProvider.get();
         if (execContext) {
@@ -18,16 +47,12 @@ export default class ExecutionContextManager {
         }
     }
 
+    /**
+     * Sets the {@link ExecutionContext}
+     * @param {ExecutionContext} execCtx {@link ExecutionContext} to be set
+     */
     static set(execCtx: ExecutionContext) {
         return contextProvider.set(execCtx);
-    }
-
-    static init() {
-        contextProvider.init();
-    }
-
-    static setProvider(provider: any) {
-        contextProvider = provider;
     }
 
 }
