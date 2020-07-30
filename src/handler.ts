b88083bae3944d81778d88398a42abc8028db4bc
Serkan ÖZAL <serkan@thundra.io>
2020-07-31 01:24:27 +0300

    Added comment to `handler.ts`

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,7 @@
+/**
+ * Provides Thundra handler to wrap the original AWS Lambda handler
+ */
+
 'use strict';
 
 const thundra = require('@thundra/core');
@@ -11,6 +15,13 @@ const userHandler = thundra.loadUserHandler();
 // Wrap user handler with Lambda wrapper
 const wrappedUserHandler = lambdaWrapper(userHandler);
 
+/**
+ *
+ * @param event the AWS Lambda invocation the event
+ * @param context the AWS Lambda invocation context
+ * @param callback the AWS Lambda invocation callback
+ * @return the wrapped handler
+ */
 // Export wrapper user handler
 exports.wrapper = (event: any, context: any, callback: any) => {
     return wrappedUserHandler(event, context, callback);
