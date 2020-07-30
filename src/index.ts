2584932a416ab6462dd1b979e0d5ef0cf6482c6f
Serkan ÖZAL <serkan@thundra.io>
2020-07-31 01:24:56 +0300

    Added comment to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/**
+ * The entry point of Thundra Node.js agent
+ */
+
 import ConfigProvider from './config/ConfigProvider';
 import config from './plugins/config';
 import listeners from './plugins/listeners';
@@ -14,16 +18,30 @@ import LogManager from './plugins/LogManager';
 
 let initialized = false;
 
+/**
+ * Initialized agent with given options (configs, etc ...)
+ * @param options the options (configs, etc ...) to initialize agent
+ */
 function init(options?: any) {
     ConfigProvider.init(options);
     initialized = true;
 }
 
+/**
+ * Creates {@link LambdaWrapper} to wrap the original AWS Lambda handler
+ * @param options the options (configs, etc ...) to initialize agent
+ * @return {LambdaWrapper} the AWS Lambda wrapper to wrap the original handler
+ */
 function createLambdaWrapper(options?: any) {
     init(options);
     return LambdaWrapper.createWrapper();
 }
 
+/**
+ * Wraps the given original AWS Lambda handler
+ * @param handler the original AWS Lambda handler to be wrapped
+ * @return the wrapped handler
+ */
 function lambdaWrapper(handler: any) {
     if (!initialized) {
         // If not initialized yet, init without any option
@@ -33,10 +51,18 @@ function lambdaWrapper(handler: any) {
     return wrapper(handler);
 }
 
+/**
+ * Creates {@link Logger} with given options
+ * @param options the options (configs, etc ...) to initialize logger to be created
+ */
 function createLogger(options: any) {
     return LogManager.createLogger(options);
 }
 
+/**
+ * Loads and returns the user AWS Lambda handler
+ * @return the loaded user AWS Lambda handler
+ */
 function loadUserHandler() {
     return loadHandler(
         Utils.getEnvVar(EnvVariableKeys.LAMBDA_TASK_ROOT),
@@ -44,10 +70,18 @@ function loadUserHandler() {
     );
 }
 
+/**
+ * Adds given log listener
+ * @param listener the log listener to be added
+ */
 function addLogListener(listener: any) {
     LogManager.addListener(listener);
 }
 
+/**
+ * Gets the tracer
+ * @return {Tracer} the tracer
+ */
 function tracer() {
     return ExecutionContextManager.get().tracer;
 }
