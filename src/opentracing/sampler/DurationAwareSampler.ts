a82e271048efc3a1ec7554269b067fc7ecb0cdab
Serkan ÖZAL <serkan@thundra.io>
2020-08-01 00:13:56 +0300

    Added comments to `DurationAwareSampler.ts`

--- src/opentracing/sampler/DurationAwareSampler.ts
+++ src/opentracing/sampler/DurationAwareSampler.ts
@@ -1,15 +1,24 @@
 import Sampler from './Sampler';
 import ThundraSpan from '../Span';
 
+/**
+ * {@link Sampler} implementation which samples {@link ThundraSpan}s
+ * if their durations in milliseconds are in the allowed side (greater than or not)
+ * according to given duration in milliseconds.
+ */
 class DurationAwareSampler implements Sampler<ThundraSpan> {
-    duration: number;
-    longerThan: boolean;
+
+    private duration: number;
+    private longerThan: boolean;
 
     constructor(duration: number, longerThan: boolean) {
         this.duration = duration;
         this.longerThan = longerThan;
     }
 
+    /**
+     * @inheritDoc
+     */
     isSampled(span: ThundraSpan): boolean {
         if (span) {
             if (this.longerThan) {
@@ -21,6 +30,7 @@ class DurationAwareSampler implements Sampler<ThundraSpan> {
             return false;
         }
     }
+
 }
 
 export default DurationAwareSampler;
