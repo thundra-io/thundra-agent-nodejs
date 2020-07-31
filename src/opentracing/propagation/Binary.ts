f7ad666ed17718431a5717e72658a7f381712e7c
Serkan ÖZAL <serkan@thundra.io>
2020-07-31 17:06:21 +0300

    Added comments to `Binary.ts`

--- src/opentracing/propagation/Binary.ts
+++ src/opentracing/propagation/Binary.ts
@@ -1,12 +1,31 @@
 import ThundraSpanContext from '../SpanContext';
 
+/**
+ * Propagator to inject/extract {@link ThundraSpanContext}
+ * into/from carrier as binary
+ */
 class BinaryPropagator {
-    // tslint:disable-next-line:no-empty
-    inject(spanContext: ThundraSpanContext, carrier: any) {}
 
+    /**
+     * Injects given {@link ThundraSpanContext} into carrier as binary
+     * @param spanContext {@link ThundraSpanContext} to be injected
+     * @param carrier the carried to be injected into
+     */
+    // tslint:disable-next-line:no-empty
+    inject(spanContext: ThundraSpanContext, carrier: any) {
+        // Not supported yet
+    }
+
+    /**
+     * Extracts {@link ThundraSpanContext} from carrier as binary
+     * @param carrier the carrier to be extracted from
+     * @return the extracted {@link ThundraSpanContext}
+     */
     extract(carrier: any): any {
+        // Not supported yet
         return null;
     }
+
 }
 
 export default BinaryPropagator;
