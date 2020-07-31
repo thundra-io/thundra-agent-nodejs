af369e6c6eb0eb5906b5d165c80520ba2db54af6
Serkan ÖZAL <serkan@thundra.io>
2020-07-31 17:06:38 +0300

    Added comments to `Http.ts`

--- src/opentracing/propagation/Http.ts
+++ src/opentracing/propagation/Http.ts
@@ -1,5 +1,9 @@
 import TextMapPropagator from './TextMap';
 
+/**
+ * Propagator to inject/extract {@link ThundraSpanContext}
+ * into/from HTTP request through headers
+ */
 class HttpPropagator extends TextMapPropagator {}
 
 export default HttpPropagator;
