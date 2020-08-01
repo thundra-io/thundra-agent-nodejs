2808527298b645fded364d1f0cb2af3e9beb38d0
Serkan ÖZAL <serkan@thundra.io>
2020-08-01 10:37:24 +0300

    Added comments ro `HttpError.ts`

--- src/error/HttpError.ts
+++ src/error/HttpError.ts
@@ -1,4 +1,8 @@
+/**
+ * Represents HTTP operation related errors
+ */
 class HttpError extends Error {
+
     constructor(message: string) {
         super();
         this.message = message;
@@ -6,6 +10,7 @@ class HttpError extends Error {
         Error.captureStackTrace(this, HttpError);
         Object.setPrototypeOf(this, HttpError.prototype);
     }
+
 }
 
 export default HttpError;
