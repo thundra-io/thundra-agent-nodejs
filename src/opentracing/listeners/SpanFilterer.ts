89bb54b5881c0520e43e46a3fbf604c22a619565
Serkan ÖZAL <serkan@thundra.io>
2020-08-01 09:43:35 +0300

    Added comments to `SpanFilterer.ts`

--- src/opentracing/listeners/SpanFilterer.ts
+++ src/opentracing/listeners/SpanFilterer.ts
@@ -1,7 +1,17 @@
 import ThundraSpan from '../Span';
 
+/**
+ * Applies {@link SpanFilter}s to {@link ThundraSpan} to decided whether it should be filtered
+ */
 interface SpanFilterer {
+
+    /**
+     * Decides whether the given {@link ThundraSpan} should be filtered
+     * @param {ThundraSpan} span the {@link ThundraSpan} to be decided whether it should filtered or not
+     * @return {@code true} if the {@link ThundraSpan} should filtered, {@code false} otherwise
+     */
     accept(span: ThundraSpan): boolean;
+
 }
 
 export default SpanFilterer;
