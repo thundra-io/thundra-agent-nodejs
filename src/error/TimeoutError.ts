e9168e23a6eaf9a5b1b9d5a9c9086fb95740b73d
Serkan ÖZAL <serkan@thundra.io>
2020-08-01 10:38:15 +0300

    Added comments for `TimeoutError.ts`

--- src/error/TimeoutError.ts
+++ src/error/TimeoutError.ts
@@ -1,4 +1,8 @@
+/**
+ * Represents timeout errors (for ex. AWS Lambda invocation timeout)
+ */
 class TimeoutError extends Error {
+
     constructor(message: string) {
         super();
         this.message = message;
@@ -6,6 +10,7 @@ class TimeoutError extends Error {
         Error.captureStackTrace(this, TimeoutError);
         Object.setPrototypeOf(this, TimeoutError.prototype);
     }
+
 }
 
 export default TimeoutError;
