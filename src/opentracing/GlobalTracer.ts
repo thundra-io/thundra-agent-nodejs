fd60f45177681ee59ffb96c8e6a889aaea952422
Serkan ÖZAL <serkan@thundra.io>
2020-08-01 21:17:37 +0300

    Added comments to `GlobalTracer.ts`

--- src/opentracing/GlobalTracer.ts
+++ src/opentracing/GlobalTracer.ts
@@ -2,19 +2,41 @@ import { Tracer } from 'opentracing';
 import { SpanContext } from 'opentracing';
 import ExecutionContextManager from '../context/ExecutionContextManager';
 
+/**
+ * Thundra's global {@link Tracer} implementation
+ */
 class GlobalTracer extends Tracer {
+
     constructor() {
         super();
     }
 
+    /**
+     * Starts {@link Span}
+     * @param {string} the name name of the span
+     * @param fields the fields to be used while creating {@link Span}
+     * @return {Span} the started {@link Span}
+     */
     startSpan(name: any, fields: any) {
         return this._startSpan(name, fields);
     }
 
-    inject(spanContext: any, format: any, carrier: any): Tracer {
+    /**
+     * Injects given {@link SpanContext} into carrier
+     * @param spanContext {@link SpanContext} to be injected
+     * @param format the format of the data to be injected
+     * @param carrier the carrier to be injected into
+     */
+    inject(spanContext: SpanContext, format: any, carrier: any): Tracer {
         return this._inject(spanContext, format, carrier);
     }
 
+    /**
+     * Extracts {@link SpanContext} from carrier
+     * @param format the format of the data to be extracted
+     * @param carrier the carrier to be extracted from
+     * @return the extracted {@link SpanContext}
+     */
     extract(format: any, carrier: any): SpanContext {
         return this._extract(format, carrier);
     }
@@ -42,6 +64,7 @@ class GlobalTracer extends Tracer {
         }
         return tracer._extract(format, carrier);
     }
+
 }
 
 export default GlobalTracer;
