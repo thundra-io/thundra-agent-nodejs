c6a0001f95726679a0b2b75c6f1c60934f2f6611
Serkan ÖZAL <serkan@thundra.io>
2020-08-04 14:15:20 +0300

    Added comments to `MonitoringDataType.ts`

--- src/plugins/data/base/MonitoringDataType.ts
+++ src/plugins/data/base/MonitoringDataType.ts
@@ -1,9 +1,14 @@
+/**
+ * Defines monitoring data types
+ */
 enum MonitoringDataType {
+
     INVOCATION = 'Invocation',
     SPAN = 'Span',
     METRIC = 'Metric',
     LOG = 'Log',
     COMPOSITE = 'Composite',
+
 }
 
 export default MonitoringDataType;
