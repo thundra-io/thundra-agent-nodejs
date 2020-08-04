5c3fca59af6cda47afd2383afd723ed23aac4a3e
Serkan ÖZAL <serkan@thundra.io>
2020-08-04 18:43:50 +0300

    Added comments to `BasePluginConfig.ts`

--- src/plugins/config/BasePluginConfig.ts
+++ src/plugins/config/BasePluginConfig.ts
@@ -1,9 +1,14 @@
+/**
+ * Base class for plugin configurations
+ */
 class BasePluginConfig {
+
     enabled: boolean;
 
     constructor(enabled: boolean) {
         this.enabled = enabled;
     }
+
 }
 
 export default BasePluginConfig;
