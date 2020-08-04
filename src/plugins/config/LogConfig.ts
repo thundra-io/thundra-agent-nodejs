08806dc3792839484ed845e7b20bc35d719f9ee3
Serkan ÖZAL <serkan@thundra.io>
2020-08-04 18:56:09 +0300

    Added comments to `LogConfig.ts`

--- src/plugins/config/LogConfig.ts
+++ src/plugins/config/LogConfig.ts
@@ -2,7 +2,11 @@ import BasePluginConfig from './BasePluginConfig';
 import Sampler from '../../opentracing/sampler/Sampler';
 const get = require('lodash.get');
 
+/**
+ * Configures log plugin/support
+ */
 class LogConfig extends BasePluginConfig {
+
     sampler: Sampler<any>;
 
     constructor(options: any) {
@@ -11,10 +15,15 @@ class LogConfig extends BasePluginConfig {
         this.sampler = options.sampler;
     }
 
+    /**
+     * Updates configuration by options
+     * @param options the options to update configuration
+     */
     updateConfig(options: any) {
         this.sampler = get(options, 'logConfig.sampler', this.sampler);
         this.enabled = get(options, 'logConfig.enabled', this.enabled);
     }
+
 }
 
 export default LogConfig;
