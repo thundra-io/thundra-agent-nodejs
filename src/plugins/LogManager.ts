5331cb0ec00bed1b84bbd41db90af3b2c0aa4dcd
Serkan ÖZAL <serkan@thundra.io>
2020-08-05 00:09:42 +0300

    Added comments to `LogManager.ts`

--- src/plugins/LogManager.ts
+++ src/plugins/LogManager.ts
@@ -1,27 +1,51 @@
 import Logger from './Logger';
 
+/**
+ * Mediator class for log related operations
+ */
 class LogManager {
-    static listeners: any[] = [];
 
+    private static listeners: any[] = [];
+
+    private constructor() {
+    }
+
+    /**
+     * Adds the log listener to be invoked by reported logs
+     * @param listener the log listener to be invoked by reported logs
+     */
     static addListener(listener: any) {
         if (!LogManager.listeners.includes(listener)) {
             LogManager.listeners = [...LogManager.listeners, listener];
         }
     }
 
+    /**
+     * Creates {@link Logger} to log through
+     * @param options the options
+     * @return {Logger} {@link Logger} to log through
+     */
     static createLogger(options: any) {
         return new Logger(options);
     }
 
+    /**
+     * Reports log
+     * @param logReport the log to be reported
+     */
     static reportLog(logReport: any) {
         LogManager.listeners.forEach((listener) => {
             listener.reportLog(logReport);
         });
     }
 
+    /**
+     * Destroys the log manager
+     */
     static destroy() {
         LogManager.listeners = [];
     }
+
 }
 
 export default LogManager;
