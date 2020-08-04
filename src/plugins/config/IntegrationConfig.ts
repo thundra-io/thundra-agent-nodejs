017924a15802759f60cdad3f8aa5288b18355d04
Serkan ÖZAL <serkan@thundra.io>
2020-08-04 18:54:06 +0300

    Added comments to `IntegrationConfig.ts`

--- src/plugins/config/IntegrationConfig.ts
+++ src/plugins/config/IntegrationConfig.ts
@@ -1,13 +1,20 @@
 import BasePluginConfig from './BasePluginConfig';
 const get = require('lodash.get');
+
+/**
+ * Configures integration
+ */
 class IntegrationConfig  extends BasePluginConfig {
+
     name: string;
     options: any;
+
     constructor(name: string, options: any) {
         super(get(options, 'enabled', true));
         this.name = name;
         this.options = options;
     }
+
 }
 
 export default IntegrationConfig;
