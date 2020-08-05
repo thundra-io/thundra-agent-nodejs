01efcb8ece1374025bb0f4030982efc78e670b0b
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-08-05 15:06:26 +0300

    Move common methods to wrapper utils

---
+++ src/wrappers/WrapperUtils.ts
@@ -0,0 +1,98 @@
+import InvocationPlugin from '../plugins/Invocation';
+import TracePlugin from '../plugins/Trace';
+import LogPlugin from '../plugins/Log';
+import ExecutionContextManager from '../context/ExecutionContextManager';
+import Reporter from '../Reporter';
+import ThundraLogger from '../ThundraLogger';
+import PluginContext from '../plugins/PluginContext';
+import ConfigProvider from '../config/ConfigProvider';
+import ThundraConfig from '../plugins/config/ThundraConfig';
+import ConfigNames from '../config/ConfigNames';
+import { ApplicationManager } from '../application/ApplicationManager';
+import ExecutionContext from '../context/ExecutionContext';
+import Utils from '../utils/Utils';
+import ThundraTracer from '../opentracing/Tracer';
+import * as asyncContextProvider from '../context/asyncContextProvider';
+
+const get = require('lodash.get');
+
+export default class WrapperUtils {
+    static beforeRequest(plugins: any[]) {
+        const context = ExecutionContextManager.get();
+        for (const plugin of plugins) {
+            plugin.beforeInvocation(context);
+        }
+    }
+
+    static async afterRequest(plugins: any[], reporter: Reporter) {
+        const context = ExecutionContextManager.get();
+        context.finishTimestamp = Date.now();
+
+        for (const plugin of plugins) {
+            plugin.afterInvocation(context);
+        }
+
+        try {
+            await reporter.sendReports(context.reports);
+        } catch (err) {
+            ThundraLogger.error(err);
+        }
+    }
+
+    static createPlugins(config: ThundraConfig, pluginContext: PluginContext): any[] {
+        const plugins: any[] = [];
+        if (config.disableMonitoring) {
+            return plugins;
+        }
+
+        if (!ConfigProvider.get<boolean>(ConfigNames.THUNDRA_TRACE_DISABLE) && config.traceConfig.enabled) {
+            const tracePlugin = new TracePlugin(config.traceConfig);
+            plugins.push(tracePlugin);
+        }
+
+        if (!ConfigProvider.get<boolean>(ConfigNames.THUNDRA_LOG_DISABLE) && config.logConfig.enabled) {
+            plugins.push(new LogPlugin(config.logConfig));
+        }
+
+        const invocationPlugin = new InvocationPlugin(config.invocationConfig);
+        plugins.push(invocationPlugin);
+
+        // Set plugin context for plugins
+        plugins.forEach((plugin: any) => { plugin.setPluginContext(pluginContext); });
+
+        return plugins;
+    }
+
+    static createPluginContext(apiKey: string, executor: any): PluginContext {
+        const applicationInfo = ApplicationManager.getApplicationInfo();
+
+        return new PluginContext({
+            applicationInfo,
+            apiKey,
+            executor,
+        });
+    }
+
+    static initAsyncContextManager() {
+        ExecutionContextManager.setProvider(asyncContextProvider);
+        ExecutionContextManager.init();
+    }
+
+    static createExecContext(): ExecutionContext {
+        const { thundraConfig } = ConfigProvider;
+        const tracerConfig = get(thundraConfig, 'traceConfig.tracerConfig', {});
+
+        const tracer = new ThundraTracer(tracerConfig);
+        const transactionId = Utils.generateId();
+
+        tracer.setTransactionId(transactionId);
+
+        const startTimestamp = Date.now();
+
+        return new ExecutionContext({
+            tracer,
+            transactionId,
+            startTimestamp,
+        });
+    }
+}
--- src/wrappers/express/ExpressWrapper.ts
+++ src/wrappers/express/ExpressWrapper.ts
@@ -1,18 +1,9 @@
-import InvocationPlugin from '../../plugins/Invocation';
-import TracePlugin from '../../plugins/Trace';
-import LogPlugin from '../../plugins/Log';
-import PluginContext from '../../plugins/PluginContext';
 import Reporter from '../../Reporter';
-import ConfigNames from '../../config/ConfigNames';
-import ThundraTracer from '../../opentracing/Tracer';
 import ConfigProvider from '../../config/ConfigProvider';
 import { ApplicationManager } from '../../application/ApplicationManager';
 import ExecutionContextManager from '../../context/ExecutionContextManager';
 import * as ExpressExecutor from './ExpressExecutor';
-import * as asyncContextProvider from '../../context/asyncContextProvider';
-import ExecutionContext from '../../context/ExecutionContext';
-import Utils from '../../utils/Utils';
-import ThundraLogger from '../../ThundraLogger';
+import WrapperUtils from '../WrapperUtils';
 
 const get = require('lodash.get');
 
@@ -23,20 +14,21 @@ export function expressMW() {
         applicationDomainName: 'API',
     });
 
-    const { apiKey } = ConfigProvider.thundraConfig;
+    const config = ConfigProvider.thundraConfig;
+    const { apiKey } = config;
     const reporter = new Reporter(apiKey);
-    const pluginContext = createPluginContext(apiKey);
-    const plugins = createPlugins(pluginContext);
+    const pluginContext = WrapperUtils.createPluginContext(apiKey, ExpressExecutor);
+    const plugins = WrapperUtils.createPlugins(config, pluginContext);
 
-    initContextManager();
+    WrapperUtils.initAsyncContextManager();
 
     return (req: any, res: any, next: any) => ExecutionContextManager.runWithContext(
-        createExecContext,
+        WrapperUtils.createExecContext,
         () => {
             try {
-                beforeRequest(plugins);
+                WrapperUtils.beforeRequest(plugins);
                 res.once('finish', () => {
-                    afterRequest(plugins, reporter);
+                    WrapperUtils.afterRequest(plugins, reporter);
                 });
             } catch (err) {
                 console.error(err);
@@ -45,85 +37,4 @@ export function expressMW() {
             }
         },
     );
-}
-
-function initContextManager() {
-    ExecutionContextManager.setProvider(asyncContextProvider);
-    ExecutionContextManager.init();
-}
-
-function createExecContext(): ExecutionContext {
-    const { thundraConfig } = ConfigProvider;
-    const tracerConfig = get(thundraConfig, 'traceConfig.tracerConfig', {});
-
-    const tracer = new ThundraTracer(tracerConfig);
-    const transactionId = Utils.generateId();
-
-    tracer.setTransactionId(transactionId);
-
-    const startTimestamp = Date.now();
-
-    return new ExecutionContext({
-        tracer,
-        transactionId,
-        startTimestamp,
-    });
-}
-
-function createPluginContext(apiKey: string): PluginContext {
-    const applicationInfo = ApplicationManager.getApplicationInfo();
-
-    return new PluginContext({
-        applicationInfo,
-        apiKey,
-        executor: ExpressExecutor,
-    });
-}
-
-function createPlugins(pluginContext: PluginContext): any[] {
-    const { thundraConfig } = ConfigProvider;
-
-    const plugins: any[] = [];
-    if (thundraConfig.disableMonitoring) {
-        return plugins;
-    }
-
-    if (!ConfigProvider.get<boolean>(ConfigNames.THUNDRA_TRACE_DISABLE) && thundraConfig.traceConfig.enabled) {
-        const tracePlugin = new TracePlugin(thundraConfig.traceConfig);
-        plugins.push(tracePlugin);
-    }
-
-    if (!ConfigProvider.get<boolean>(ConfigNames.THUNDRA_LOG_DISABLE) && thundraConfig.logConfig.enabled) {
-        plugins.push(new LogPlugin(thundraConfig.logConfig));
-    }
-
-    const invocationPlugin = new InvocationPlugin(thundraConfig.invocationConfig);
-    plugins.push(invocationPlugin);
-
-    // Set plugin context for plugins
-    plugins.forEach((plugin: any) => { plugin.setPluginContext(pluginContext); });
-
-    return plugins;
-}
-
-function beforeRequest(plugins: any[]) {
-    const context = ExecutionContextManager.get();
-    for (const plugin of plugins) {
-        plugin.beforeInvocation(context);
-    }
-}
-
-async function afterRequest(plugins: any[], reporter: Reporter) {
-    const context = ExecutionContextManager.get();
-    context.finishTimestamp = Date.now();
-
-    for (const plugin of plugins) {
-        plugin.afterInvocation(context);
-    }
-
-    try {
-        await reporter.sendReports(context.reports);
-    } catch (err) {
-        ThundraLogger.error(err);
-    }
 }
