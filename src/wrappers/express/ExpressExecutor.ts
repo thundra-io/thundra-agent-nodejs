97a27e8ca894fb010df3470603367cd0d34c0cd6
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-09-14 18:01:10 +0300

    Update ExpressExecutor.ts (#240)

--- src/wrappers/express/ExpressExecutor.ts
+++ src/wrappers/express/ExpressExecutor.ts
@@ -16,6 +16,9 @@ export function finishInvocation(pluginContext: PluginContext, execContext: any)
 
 export function startTrace(pluginContext: PluginContext, execContext: ExecutionContext) {
     const { request } = execContext;
+
+    setupRoutePathHandler(execContext);
+
     WrapperUtils.startTrace(pluginContext, execContext);
 
     const { rootSpan, triggerOperationName, response } = execContext;
@@ -58,4 +61,35 @@ export function finishTrace(pluginContext: PluginContext, execContext: Execution
 
     Utils.copyProperties(response, ['statusCode'], rootSpan.tags, [HttpTags.HTTP_STATUS]);
     Utils.copyProperties(request.route, ['path'], rootSpan.tags, [HttpTags.HTTP_ROUTE_PATH]);
+}
+
+function handleRoutePath(context: ExecutionContext, resourceName: string) {
+    const { rootSpan, request, response } = context;
+
+    const triggerOperationName = request.hostname + resourceName;
+    context.triggerOperationName = triggerOperationName;
+
+    // Change root span name and response header
+    rootSpan.operationName = resourceName;
+    response.set(TriggerHeaderTags.RESOURCE_NAME, triggerOperationName);
+}
+
+function setupRoutePathHandler(execContext: ExecutionContext) {
+    const { request } = execContext;
+
+    if (!request.route) {
+        Object.defineProperty(request, 'route', {
+            get() {
+                return request._route;
+            },
+            set(newValue) {
+                request._route = newValue;
+                if (request._route) {
+                    handleRoutePath(execContext, request._route.path);
+                }
+            },
+        });
+    } else {
+        handleRoutePath(execContext, request.route.path);
+    }
 }
