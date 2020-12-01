a3c1d2a8919c4a13cb955d9a08b92042947c7509
Serkan ÖZAL <serkan@thundra.io>
2020-12-01 12:24:45 +0300

    Prevented telemetry data reporting and finishing multiple times (#256)

--- src/plugins/data/invocation/InvocationData.ts
+++ src/plugins/data/invocation/InvocationData.ts
@@ -33,6 +33,31 @@ class InvocationData extends BaseMonitoringData {
     }
 
     /**
+     * Checks whether invocation has finished
+     *
+     * @return {@code true} invocation has finished,
+     *         {@code false} otherwise
+     */
+    isFinished(): boolean {
+        return this.finishTimestamp > 0;
+    }
+
+    /**
+     * Finished invocation if it is not finished yet
+     * @param {number} finishTimestamp the optional finish timestamp.
+     *        If it is not specified, current time will be used
+     */
+    finish(finishTimestamp?: number): void {
+        if (!this.isFinished()) {
+            if (!finishTimestamp) {
+                finishTimestamp = Date.now();
+            }
+            this.finishTimestamp = finishTimestamp;
+            this.duration = finishTimestamp - this.startTimestamp;
+        }
+    }
+
+    /**
      * Sets tags to the invocation
      * @param tags the tags to be set
      */
