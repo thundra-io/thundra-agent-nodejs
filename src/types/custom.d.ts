86e6fc46960c62f3389a014fccccd28dadd7b124
Serkan ÖZAL <serkan@thundra.io>
2020-12-08 21:11:13 +0300

    Delegated to master (loaded first) module from later loaded modules in case of there are multiple Thundra modules coming from different directories

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -5,6 +5,7 @@
 declare namespace NodeJS {
     export interface Global {
         __thundraImports__: any;
+        __thundraMasterModule__: any;
         __thundraTraceEntry__: any;
         __thundraTraceLine__: any;
         __thundraTraceExit__: any;
