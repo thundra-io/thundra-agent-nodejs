751df48a757a54000628374f16dbfac5137ada15
Fatih Aydilek <fatihaydilek@gmail.com>
2021-09-03 16:17:31 +0300

    catch non-exist modules while instrumenting

--- src/wrappers/foresight/lib/jest/ModuleLoader.ts
+++ src/wrappers/foresight/lib/jest/ModuleLoader.ts
@@ -1,24 +1,21 @@
 import ModuleUtils from '../../../../utils/ModuleUtils';
 import {INTEGRATIONS} from '../../../../Constants';
+import ThundraLogger from '../../../../ThundraLogger';
 
 const LoadTestModules = (testRequire: any) => {
-
-    /**
-     * todo: take integration modules list
-     * and call instrumentModule method for all integration modules
-     */
-
     for (const key in INTEGRATIONS) {
         const integration = INTEGRATIONS[key];
         if (integration) {
-            for (const module of integration.moduleNames){
-                ModuleUtils.instrumentModule(module, testRequire(module));
+            for (const module of integration.moduleNames) {
+                try {
+                    ModuleUtils.instrumentModule(module, testRequire(module));
+                    ThundraLogger.info(`<ModuleLoader> Module instrumented: ${module}`);
+                } catch (e) {
+                    ThundraLogger.info(`<ModuleLoader> Cannot instrument module: ${module}`);
+                }
             }
         }
     }
-    // ModuleUtils.instrumentModule('aws-sdk', testRequire('aws-sdk'));
-    // ModuleUtils.instrumentModule('aws-sdk/lib/core.js', testRequire('aws-sdk/lib/core.js'));
-    // ModuleUtils.instrumentModule('redis', testRequire('redis'));
 };
 
 export default LoadTestModules;
