f4c8d64c3897bb2be93e880164d6f90880dbfac9
Serkan ÖZAL <serkan@thundra.io>
2021-11-04 15:17:54 +0300

    Introduced bootstrap file for AWS Lambda environment (#307)
    * Introduced bootstrap file for AWS Lambda environment
    
    * Changes on package require paths
    
    * Change 'thundraBridge' require path to relative path
    
    Co-authored-by: batuhan <batuhancaglayan1@gmail.com>

---
+++ src/bootstrap/lambda/index.js
@@ -0,0 +1,25 @@
+const thundra = require('../../thundraBridge');
+
+const ORIGINAL_HANDLER_ENV_VAR_NAME = '_HANDLER';
+const THUNDRA_HANDLER_ENV_VAR_NAME = 'THUNDRA_AGENT_LAMBDA_HANDLER';
+const LAMBDA_TASK_ROOT_ENV_VAR_NAME = 'LAMBDA_TASK_ROOT';
+const JS_FILE_EXTENSION = '.js';
+
+// Get user handler
+const userHandler = process.env[ORIGINAL_HANDLER_ENV_VAR_NAME];
+
+// Resolve Thundra handler
+const lambdaTaskRoot = process.env[LAMBDA_TASK_ROOT_ENV_VAR_NAME];
+let thundraHandler = require.resolve('@thundra/core/dist/handler');
+if (thundraHandler.startsWith(lambdaTaskRoot)) {
+    thundraHandler = '.' + thundraHandler.substring(lambdaTaskRoot.length);
+}
+if (thundraHandler.endsWith(JS_FILE_EXTENSION)) {
+    thundraHandler = thundraHandler.substring(0, thundraHandler.length - JS_FILE_EXTENSION.length);
+}
+
+// Switch user handler with Thundra handler
+process.env[ORIGINAL_HANDLER_ENV_VAR_NAME] = thundraHandler + '.wrapper';
+process.env[THUNDRA_HANDLER_ENV_VAR_NAME] = userHandler;
+
+thundra.init();
