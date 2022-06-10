afe94fd955171907cdbd31da90c971296707350c
Serkan ÖZAL <serkan@thundra.io>
2022-06-10 17:54:11 +0300

    Fixed default export problem in `index.d.ts` to be able to import in ES modules (#357)

--- src/index.d.ts
+++ src/index.d.ts
@@ -19,7 +19,7 @@ declare module '@thundra/core' {
    * @param options the options (configs, etc ...) to initialize agent
    * @return {LambdaWrapper} the AWS Lambda wrapper to wrap the original handler
    */
-  export function createLambdaWrapper(options?: any): <F extends Function>(f: F) => F;
+  export default function createLambdaWrapper(options?: any): <F extends Function>(f: F) => F;
 
   /**
    * Wraps the given original AWS Lambda handler
@@ -195,6 +195,4 @@ declare module '@thundra/core' {
    * automatically from the base class.
    */
   export class Tracer { }
-
-  export = createLambdaWrapper;
 }
